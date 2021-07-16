07c13d890636fb5d5b21754432562643b4270fa0
Siyan Guo <siyang@Hey-There.local>
2021-07-16 19:45:04 -0400

    use Jquery to create timeblocks

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,34 @@ var today = moment().format('dddd, MMMM Do');
 $("#currentDay").text(today);
 
 var schedules = [];
+var timeBlocks = ["9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm"]
+
+//create timeblocks
+for (var i = 0; i < timeBlocks.length; i++) {
+    var timeBlockDiv = $("<div>")
+        .addClass("row time-block")
+
+    var timeText = $("<p>")
+        .addClass("col-md-2 hour")
+        .text(timeBlocks[i])
+
+    var scheduleInput = $("<textarea>")
+        .addClass("col-md-8 description")
+        .attr("cols", "100")
+        .attr("row", "3")
+        .attr("id", i)
+
+    var saveDiv = $("<div>")
+        .addClass("col-md-2 saveBtn")
+        .attr("id", i)
+
+    var icon = $("<span>")
+        .addClass("oi oi-circle-check display-4 mt-3")
+
+    $(saveDiv).append(icon);
+    $(timeBlockDiv).append(timeText, scheduleInput, saveDiv);
+    $(".container").append(timeBlockDiv);
+}
 
 //iterate each timeblock and change background color
 $(".time-block").each(function (index, el) {
@@ -22,23 +50,44 @@ $(".time-block").each(function (index, el) {
     }
 });
 
-var saveSchedules = function () {
-    var id = $(this).attr("id");
+// save schedules in an array and localStorage
+var saveSchedulesHandler = function () {
+    var id = JSON.parse($(this).attr("id"));
+    console.log("event - id", id);
+    // $("textarea[id=" + id + "]").change(function () {
+    //     alert("changed");
+    // })
     var scheduleInput = $(this).siblings(".description").val();
-    var schedule = { id: id, name: scheduleInput };
-    schedules.push(schedule);
-    localStorage.setItem("schedules", JSON.stringify(schedules));
-    loadSchedules();
-}
-
-var loadSchedules = function () {
-    schedules = JSON.parse(localStorage.getItem("schedules"));
     console.log(schedules);
     $.each(schedules, function (index, val) {
-        console.log("val", val);
+        if (val.id === id) {
+            val.name = scheduleInput;
+            return false;
+        }
+    })
+    var schedule = { id: id, name: scheduleInput };
+    schedules.push(schedule);
+    console.log(schedule);
+    localStorage.setItem("schedules", JSON.stringify(schedules));
+    loadSchedules();
+
+}
+
+//load schedules on the page
+var loadSchedules = function () {
+    schedules = JSON.parse(localStorage.getItem("schedules"));
+    if (!schedules) {
+        schedules = [];
+    }
+    $.each(schedules, function (index, val) {
         $("textarea[id=" + val.id + "]").val(val.name)
     })
+    console.log(schedules);
 }
 
 loadSchedules();
-$(".time-block").on("click", ".saveBtn", saveSchedules);
+
+$(".time-block").on("click", ".saveBtn", saveSchedulesHandler);
+
+//reload the page every 30 mins
+// setInterval(loadSchedules(), (1000 * 60) * 30);
