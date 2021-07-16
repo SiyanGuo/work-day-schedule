6fd8fdb406d74c8b12ca9bd28e9820044231855c
Siyan Guo <siyang@Hey-There.local>
2021-07-16 13:07:42 -0400

    save data in localStorage

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,44 @@
+//get current date
 var today = moment().format('dddd, MMMM Do');
 $("#currentDay").text(today);
 
-$(".time-block").each(function(index, el){
+var schedules = [];
 
+//iterate each timeblock and change background color
+$(".time-block").each(function (index, el) {
     var workTime = $(this).find("p").text().trim();
     workTime = moment(workTime, "h:mma")
-    var diff= moment().diff(workTime, "minutes");
+    var diff = moment().diff(workTime, "minutes");
 
-    if (diff<0) {
+    if (diff < 0) {
         $(this).find("textarea").addClass("future");
         console.log("future");
-    } else if (0<diff && diff<60){
+    } else if (0 < diff && diff < 60) {
         $(this).find("textarea").addClass("present");
         console.log("present");
-    } else{
+    } else {
         $(this).find("textarea").addClass("past");
         console.log("past");
     }
+});
 
-})
+var saveSchedules = function () {
+    var id = $(this).attr("id");
+    var scheduleInput = $(this).siblings(".description").val();
+    var schedule = { id: id, name: scheduleInput };
+    schedules.push(schedule);
+    localStorage.setItem("schedules", JSON.stringify(schedules));
+    loadSchedules();
+}
+
+var loadSchedules = function () {
+    schedules = JSON.parse(localStorage.getItem("schedules"));
+    console.log(schedules);
+    $.each(schedules, function (index, val) {
+        console.log("val", val);
+        $("textarea[id=" + val.id + "]").val(val.name)
+    })
+}
+
+loadSchedules();
+$(".time-block").on("click", ".saveBtn", saveSchedules);
