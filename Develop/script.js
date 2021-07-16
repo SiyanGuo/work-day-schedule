46302c780b85e2e831ea6979f4258de47b403ca9
Siyan Guo <siyang@Hey-There.local>
2021-07-16 12:07:41 -0400

    color-coded time block

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,21 @@
 var today = moment().format('dddd, MMMM Do');
 $("#currentDay").text(today);
 
+$(".time-block").each(function(index, el){
+
+    var workTime = $(this).find("p").text().trim();
+    workTime = moment(workTime, "h:mma")
+    var diff= moment().diff(workTime, "minutes");
+
+    if (diff<0) {
+        $(this).find("textarea").addClass("future");
+        console.log("future");
+    } else if (0<diff && diff<60){
+        $(this).find("textarea").addClass("present");
+        console.log("present");
+    } else{
+        $(this).find("textarea").addClass("past");
+        console.log("past");
+    }
+
+})
